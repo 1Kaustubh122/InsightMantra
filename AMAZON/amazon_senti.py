da1e9221584279047db0b093be116242d9d5af4f
AmanSwar <155733111+AmanSwar@users.noreply.github.com>
2024-07-28 08:27:02 +0530

    good practices for amazon_senti.py

--- AMAZON/amazon_senti.py
+++ AMAZON/amazon_senti.py
@@ -1,3 +1,4 @@
+# deep learning
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch.nn.functional as F
@@ -11,10 +12,15 @@ from transformers import pipeline
 import re
 import json
 
+# your source of data in form of CSV
 DATA_DIR = "/home/aman/code/ML/demand_prd/REFACTORED /DATA/amazon_reviews.csv"
 df = pd.read_csv(DATA_DIR)
 
+
 def data_preprocess(df):
+    '''
+    preprocess the date column and make it into pandas.datetime format 
+    '''
     for text_date in df['date']:
         # Check if there's a match before attempting to access the group
         if match := re.search(r'\d{2} \w+ \d{4}', text_date):
@@ -30,14 +36,15 @@ def data_preprocess(df):
         df["date_obj"] = date_obj
         df.drop('date' , axis=1)
 
-# Assuming you have your DataFrame (df)
+# preprocessing the dataframe
 data_preprocess(df)
 
+# applying sentimentt analysis
 sentiment_analysis = pipeline("sentiment-analysis",model="siebert/sentiment-roberta-large-english")
 model_name = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model => roberta-base
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
 
 def analyze_sentiment(text):
     '''function to return sentiments mapping to given text'''
@@ -48,7 +55,6 @@ def analyze_sentiment(text):
     sentiment = torch.argmax(probs , dim=-1).item()
     sentiment_map = {0 : "negative" , 1 : "neutral" , 2  :"positive"}
     return sentiment_map[sentiment]
-
 
 def senti_score(dataset):
     '''adding labels column in the dataframe for the respective text'''
@@ -69,19 +75,23 @@ def senti_score(dataset):
     
     return list_of_senti
 
+# additional column storing all the sentiment scores correspoinding to the body
 df["labels"] = senti_score(dataset=df)
 
+# list of all the score
 all_labels = [1,0,-1]
 all_labels_list = list(df["labels"].values)
-
+# qty of scores in each list
 counts_of_labels = [all_labels_list.count(1) , all_labels_list.count(0) , all_labels_list.count(-1)]
 
+# making final dataframe for plotting all the derived data into graphs
 data_for_plotting = {"x" : all_labels , "y" : counts_of_labels}
 
 
 plotting_data = pd.DataFrame(data_for_plotting)
 plotting_data_json = plotting_data.to_json()
 
+# converting into json for export
 with open('plotting_data.json', 'w') as f:
     json.dump(plotting_data_json, f)
 
